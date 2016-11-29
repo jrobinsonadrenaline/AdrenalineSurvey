884964765ebc1a966f69c55095a51d38b08b03f0
jrobinson <jrobinson@adrenalineshot.com>
2016-11-29 17:26:08 -0500

    initial push

---
+++ AdrenalineSurvey/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  AdrenalineSurvey
+//
+//  Created by Josh Robinson on 11/29/16.
+//  Copyright © 2016 Adrenaline Inc. All rights reserved.
+//
+
+import Foundation
---
+++ AdrenalineSurvey/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RequestAccessViewController.swift
+//  AdrenalineSurvey
+//
+//  Created by Josh Robinson on 11/29/16.
+//  Copyright © 2016 Adrenaline Inc. All rights reserved.
+//
+
+import UIKit
+
+class RequestAccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
